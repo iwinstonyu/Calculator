f3a6f6a74b890a463da4d0fb88df5eb3a2d4a749
余文锦 <windpenguin@gmail.com>
2020-06-02 15:14:04 +0800

    [feat] 引用CMakeUtility

--- Distrib.bat
+++ Distrib.bat
@@ -3,7 +3,7 @@
 if exist Build rd /q /s Build
 mkdir Build
 cd Build
-cmake -DCMAKE_INSTALL_PREFIX=../Install -DGTEST_ROOT=G:/ThirdParty/googletest/Install ..
+cmake -DCMAKE_INSTALL_PREFIX=../Install -DGTEST_ROOT=G:/ThirdParty/googletest/Install -DCMAKE_UTILITY_ROOT=E:\GitRepo\CMakeUtility ..
 cmake --build . --config Debug --target install
 
 pause
